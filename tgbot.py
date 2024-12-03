232186eb772f9563d758b93cc598c3fb73836d43
Михаил Казицин <michaelkazitsin@MacBook-Air-Mihail.local>
2024-12-03 08:43:36 +0300

    secure apikey

--- tgbot.py
+++ tgbot.py
@@ -190,7 +190,23 @@ def main():
     init_db()
 
     # Загрузка токена бота из переменной окружения
-    TOKEN = "8010623899:AAE4KcSI5rvWzUri0ODzT6TENQiNgdnHLNc"
+
+    filename = 'apikey'
+
+    def get_file_contents(filename):
+        """ Given a filename,
+            return the contents of that file
+        """
+        try:
+            with open(filename, 'r') as f:
+                # It's assumed our file contains a single line,
+                # with our API key
+                return f.read().strip()
+        except FileNotFoundError:
+            print("'%s' file not found" % filename)
+
+    TOKEN = get_file_contents(filename)
+
     if not TOKEN:
         logger.error("Не найден токен бота. Установите переменную окружения TELEGRAM_BOT_TOKEN.")
         return
